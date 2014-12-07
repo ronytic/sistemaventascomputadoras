cc2535aabc9ae239e8e533716efd3243237aef1c
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-12-06 20:51:56 -0400

    Config Base de Datos

--- basedatos.php
+++ basedatos.php
@@ -5,6 +5,6 @@ setlocale(LC_ALL, 'esp');
 $host="localhost";
 $user="root";
 $pass="";
-$database="administracionphilips";
+$database="sistemaventascomputadoras";
 
 ?>
--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-$title="Sistema Control y Ventas de Productos PHILIPS";
+$title="Sistema Control y Ventas de Computadoras";
 $siglas="";
-$directory="administracionphilips";//ej:carp/ sin el "/"
+$directory="sistemaventascomputadoras";//ej:carp/ sin el "/"
 $url="http://localhost/";//ej:http:"localhost/"
 $lema2="";
 $lema='Productos de Calidad';
-$direccion="Zona 16 de Julio, calle 7 N 2031";
-$telefono="2860722";
+$direccion="EDIFICIO CHUQUIAGO CALLLE MEXICO Nro 1551 P.B. OF 1 TEL: 2114043 - 70642896";
+$telefono="2482516";
 
 $ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
 $tp="D3W1N8E0E4B4B4";
