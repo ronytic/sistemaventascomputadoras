85f51ef802e8a37bd1484e5dcdde1ccef047614e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-12-08 01:08:08 -0400

    Clase Menu Submenu

--- class/menu.php
+++ class/menu.php
@@ -6,9 +6,9 @@ class menu extends bd{
 		$this->campos=array("*");
 		switch ($Nivel) {
 			case 1:{return $this->getRecords("superadmin=1 and activo=1","orden");}break;
-			case 2:{return $this->getRecords("administrador=1 and activo=1","orden");}break;
-			case 3:{return $this->getRecords("inventario=1 and activo=1","orden");}break;
-			case 4:{return $this->getRecords("ventas=1 and activo=1","orden");}break;
+			case 2:{return $this->getRecords("gerente=1 and activo=1","orden");}break;
+			case 3:{return $this->getRecords("encargadoventas=1 and activo=1","orden");}break;
+			case 4:{return $this->getRecords("tecnicos=1 and activo=1","orden");}break;
 		}
 	}
 }
--- class/submenu.php
+++ class/submenu.php
@@ -6,9 +6,9 @@ class submenu extends bd{
 		$this->campos=array("*");
 		switch ($codUsuario) {
 			case 1:{return $this->getRecords("superadmin=1 and idmenu=$menu and activo=1","orden");}break;
-			case 2:{return $this->getRecords("administrador=1 and idmenu=$menu and activo=1","orden");}break;
-			case 3:{return $this->getRecords("inventario=1 and idmenu=$menu and activo=1","orden");}break;
-			case 4:{return $this->getRecords("ventas=1 and idmenu=$menu and activo=1","orden");}break;
+			case 2:{return $this->getRecords("gerente=1 and idmenu=$menu and activo=1","orden");}break;
+			case 3:{return $this->getRecords("encargadoventas=1 and idmenu=$menu and activo=1","orden");}break;
+			case 4:{return $this->getRecords("tecnicos=1 and idmenu=$menu and activo=1","orden");}break;
 		}
 	}
 }
